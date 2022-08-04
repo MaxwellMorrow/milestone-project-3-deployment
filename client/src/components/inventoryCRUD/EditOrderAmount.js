eb838e0ac10a1f21985308645e17b9da588adf31
Max <93175059+MaxwellMorrow@users.noreply.github.com>
2022-08-03 20:24:28 -0700

    fixed editinventory

--- client/src/components/inventoryCRUD/EditOrderAmount.js
+++ client/src/components/inventoryCRUD/EditOrderAmount.js
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 // functional component
 const EditOrderAmount = ({ item }) => {
 // vanilla js section
