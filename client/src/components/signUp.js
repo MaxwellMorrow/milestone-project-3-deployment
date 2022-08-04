42c859084d67584878f2935f59c7bb35cf56ed5f
Anthony Babka <anthonybabka@gmail.com>
2022-08-03 19:29:57 -0700

    cleaning code

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -1,6 +1,6 @@
 import "../styleSheets/index.css";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SignUp() {
