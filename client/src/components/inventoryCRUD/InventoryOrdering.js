56eabf4b7e9d634b7c79ca382e53913eb0366e78
Max <93175059+MaxwellMorrow@users.noreply.github.com>
2022-08-03 19:21:43 -0700

    getting on the same page

--- client/src/components/inventoryCRUD/InventoryOrdering.js
+++ client/src/components/inventoryCRUD/InventoryOrdering.js
@@ -1,99 +1,93 @@
 // imports/dependencies
-import React, { useState, useEffect } from 'react';
-import AddInventory from './Addinventory';
-import EditOrderAmount from './EditOrderAmount';
-
+import React, { useState, useEffect } from "react";
+import AddInventory from "./Addinventory";
+import EditOrderAmount from "./EditOrderAmount";
 
 // functional component
 const InventoryOrdering = () => {
-// vanilla js section:
+  // vanilla js section:
 
-    // useState section:
-const [ items, setItems] = useState([]);
-const [ orderamount, setOrderamount] = useState('');
+  // useState section:
+  const [items, setItems] = useState([]);
+  const [orderamount, setOrderamount] = useState("");
 
+  // helper functions:
 
-// helper functions:
-
-    // fetch requests:
-const getItems = async () => {
+  // fetch requests:
+  const getItems = async () => {
     try {
-        const response = await fetch('http://localhost:3070/inventory');
-        const jsonData = await response.json();
+      const response = await fetch("http://localhost:3070/inventory");
+      const jsonData = await response.json();
 
-         console.log(jsonData);
-        
-            setItems(jsonData);
-        
-        } catch (err) {
-            console.error(err.message);
-        }
-    };
+      console.log(jsonData);
 
-// useEffect section:
-useEffect(() => {
-    getItems()
-},[])
-
-
-// delete item function fetch request:
-const deleteItem = async (id) => {
-    try {
-        await fetch(`/inventory/${id}`, {
-            method: 'DELETE'
-        });
-    
-        setItems(items.filter(item => item.item_id !== id)); 
-
+      setItems(jsonData);
     } catch (err) {
-        console.error(err.message)
+      console.error(err.message);
     }
-};
+  };
 
+  // useEffect section:
+  useEffect(() => {
+    getItems();
+  }, []);
 
+  // delete item function fetch request:
+  const deleteItem = async (id) => {
+    try {
+      await fetch(`/inventory/${id}`, {
+        method: "DELETE",
+      });
+
+      setItems(items.filter((item) => item.item_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   return (
     <div>
-
-        <h2>Inventory Ordering List</h2>
-        <AddInventory />
-        <table class="center table table-sm table-light text-center" style={{border: '1px solid'}}>
-            <thead>
-                <tr>
-                    <th scope="col">Item</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Supplier</th>
-                    <th scope="col">Par on Hand</th>
-                    <th scope="col">Unit Each</th>  
-                    <th scope="col">Order Amount (cases)</th>    
-                </tr>
-            </thead>
-            <tbody class="listItems">
-                {items.map(item => (
-                    <tr key={item.item_id}>
-                        <td>{item.itemname}</td>
-                        <td>{item.category}</td>
-                        <td>{item.supplier}</td>
-                        <td>{item.paronhand}</td>
-                        <td>{item.uniteach}</td>
-                        <td>{item.orderamount}</td>
-                        <td><EditOrderAmount item={item} /></td>
-                        <button
-                            onClick={() => deleteItem(item.item_id)}
-                            type="button"
-                            class="btn btn-outline-danger btn-sm options">
-                            Delete
-                        </button>
-
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-
+      <h2>Inventory Ordering List</h2>
+      <AddInventory />
+      <table
+        class="center table table-sm table-light text-center"
+        style={{ border: "1px solid" }}
+      >
+        <thead>
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Category</th>
+            <th scope="col">Supplier</th>
+            <th scope="col">Par on Hand</th>
+            <th scope="col">Unit Each</th>
+            <th scope="col">Order Amount (cases)</th>
+          </tr>
+        </thead>
+        <tbody class="listItems">
+          {items.map((item) => (
+            <tr key={item.item_id}>
+              <td>{item.itemname}</td>
+              <td>{item.category}</td>
+              <td>{item.supplier}</td>
+              <td>{item.paronhand}</td>
+              <td>{item.uniteach}</td>
+              <td>{item.orderamount}</td>
+              <td>
+                <EditOrderAmount item={item} />
+              </td>
+              <button
+                onClick={() => deleteItem(item.item_id)}
+                type="button"
+                class="btn btn-outline-danger btn-sm options"
+              >
+                Delete
+              </button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
-
-
+  );
+};
 
 export default InventoryOrdering;
